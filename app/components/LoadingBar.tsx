20b7d39f6418b05d437a0006dfb7266dd45598fa
Juan Rodriguez <sjdonado@icloud.com>
2024-03-29 06:36:01 +0100

    refactor: loading bar with progress bar

--- app/components/LoadingBar.tsx
+++ app/components/LoadingBar.tsx
@@ -1,13 +1,45 @@
+import clsx from 'clsx';
+import { useEffect, useRef, useState } from 'react';
+
 import { useNavigation } from '@remix-run/react';
 
 export default function LoadingBar() {
   const navigation = useNavigation();
 
+  const isIdle = navigation.state === 'idle';
   const isLoading = navigation.state === 'loading';
+  const isSubmitting = navigation.state === 'submitting';
 
-  if (isLoading) {
-    return (
-      <progress className="absolute top-0 lef-0 progress z-20 w-full transition-opacity delay-200"></progress>
+  const active = !isIdle;
+
+  const ref = useRef<HTMLDivElement>(null);
+  const [animationComplete, setAnimationComplete] = useState(true);
+
+  useEffect(() => {
+    if (!ref.current) return;
+    if (active) setAnimationComplete(false);
+
+    Promise.allSettled(ref.current.getAnimations().map(({ finished }) => finished)).then(
+      () => !active && setAnimationComplete(true)
     );
-  }
+  }, [active]);
+
+  return (
+    <div
+      role="progressbar"
+      aria-hidden={!active}
+      aria-valuetext={active ? 'Loading' : undefined}
+      className="fixed inset-x-0 top-0 z-50 h-1 animate-pulse"
+    >
+      <div
+        ref={ref}
+        className={clsx('h-full bg-secondary transition-all duration-300 ease-in-out', {
+          'w-0 opacity-0 transition-none': isIdle && animationComplete,
+          'w-full': isIdle && !animationComplete,
+          'w-4/12': isSubmitting,
+          'w-10/12': isLoading,
+        })}
+      />
+    </div>
+  );
 }
